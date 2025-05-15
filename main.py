309198fe2198bcec2dee3ece11cc8d5e3b6d04bd
Norbert Hašan <supernorko@gmail.com>
2025-05-15 18:04:46 +0200

    pre-friday update

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import torch
 import librosa
 import numpy
 import matplotlib
+import torchaudio
+
+print(torch.__version__)
+print(torchaudio.__version__)
+x = torch.rand(5, 3)
+print(x)
+print(f"CUDA available: {torch.cuda.is_available()}")
+if torch.cuda.is_available():
+    print(f"CUDA version: {torch.version.cuda}")
+    print(f"GPU name: {torch.cuda.get_device_name(0)}")
